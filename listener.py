d429bda8dad48f5d33bd190471019dff7627b78b
Gianmaria di Rito <46752351+gimmydr97@users.noreply.github.com>
2022-11-07 12:47:11 +0100

    Update listener.py

--- listener.py
+++ listener.py
@@ -13,21 +13,13 @@ def compile_source_file(file_path):
 # C2 is the Ethereum mainnet to witch we ask to retrieve the desired variable
 w3 = Web3(HTTPProvider('https://evocative-stylish-isle.discover.quiknode.pro/6ad754e3368653a2665e62db8659b9f179a3ae43/'))
 
+# web3 is the Web3 reference to the chain C1 for take a reference to the bride contract
 chainAddress = 'http://127.0.0.1:8545'
 web3 = Web3(HTTPProvider(chainAddress))
 web3.eth.defaultAccount = web3.eth.accounts[0]
 
-contractPath = 'Contracts/Bridge.sol'
-#compile contract
-contractInterface = compile_source_file(contractPath)
-
-contractAddress = input()
-
-bridgeContract = web3.eth.contract(address=contractAddress, abi=contractInterface["abi"])
-
-
-
-def handle_event(event):
+#function that asks c2 for the proof related to the value searched by the request 
+def handle_event(bridgeContract,event):
 
     print("hendle the request")
     requestId = event['args']['requestId']
@@ -37,7 +29,7 @@ def handle_event(event):
 
     
     proof = w3.eth.get_proof(account,[key], blockId)
-    
+    """
     print(proof.accountProof)
     print(len(proof.accountProof))
     for elem in proof.accountProof:
@@ -46,7 +38,7 @@ def handle_event(event):
     print(len(proof.storageProof[0].proof))
     for elem in proof.storageProof[0].proof:
         print(len(elem))
-
+    """
     StateProof = [
         proof.address,
         proof.accountProof,
@@ -61,21 +53,36 @@ def handle_event(event):
     txn_receipt = web3.eth.wait_for_transaction_receipt(txt_hash)
     print(txn_receipt)
 
-async def log_loop(event_filter, poll_interval):
+async def log_loop(contract, event_filter, poll_interval):
+    #waiting for RequestLogged events
     while True:
         for e in event_filter.get_new_entries():
-            handle_event(e)
+            handle_event(contract, e)
         await asyncio.sleep(poll_interval)
 
 def main():
+    
+    print('enter the contract address')
+    contractAddress = input()
+
+    contractPath = 'Contracts/Bridge.sol'
+    #compile contract
+    contractInterface = compile_source_file(contractPath)
+
+    #reference to the contract
+    bridgeContract = web3.eth.contract(address=contractAddress, abi=contractInterface["abi"])
+
+    #event_filter for the RequestLogged event
+    event_filter = bridgeContract.events.RequestLogged.createFilter(fromBlock='latest')
+
     print('Listener has started!\nChain\t: {}\nContract: {}\nAddress\t: {}'.format(
                                   chainAddress, contractPath, contractAddress))
 
-    event_filter = bridgeContract.events.RequestLogged.createFilter(fromBlock='latest')
+    
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     try:
-        loop.run_until_complete(asyncio.gather(log_loop(event_filter, 2)))
+        loop.run_until_complete(asyncio.gather(log_loop(bridgeContract, event_filter, 2)))
     finally:
         loop.close()
     
