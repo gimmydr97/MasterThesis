ff6793ba52f72aa448b3014b4a366bf7059dcfdf
Gianmaria di Rito <46752351+gimmydr97@users.noreply.github.com>
2023-01-09 08:54:23 +0100

    Update requester.py

--- requester.py
+++ requester.py
@@ -10,9 +10,10 @@ def compile_source_file(file_path):
     return compile_source(source, output_values=["abi", "bin"], solc_version="0.6.0").get('<stdin>:Bridge') 
 
 def deploy_contract(w3, contract_interface):
+	#if you want to use bridge with sliding window or array put size parameter in the constructor() call function.
         tx_hash = w3.eth.contract(
                 abi=contract_interface['abi'],
-                bytecode=contract_interface['bin']).constructor().transact()
+                bytecode=contract_interface['bin']).constructor().transact() 
         address = w3.eth.get_transaction_receipt(tx_hash)['contractAddress']
         return address
 
